71c43e2c3319313aa91721db6302a033380e68e8
sandeepipss <57081337+sandeepipss@users.noreply.github.com>
2019-11-20 12:28:27 +0530

    Update stripe-button.component.jsx

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_llutwQxZsgpt2E8jBdGmXsSa00gEZ7smnL';
 
   const onToken = token => {
     console.log(token);
